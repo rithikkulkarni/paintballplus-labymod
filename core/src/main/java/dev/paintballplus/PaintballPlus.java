7604f0cbb70fa5236442d79412fc085601393468
rithikkulkarni <rithik.r.kulkarni@gmail.com>
2026-06-16 11:20:30 -0400

    Added PB+ Indicator on Tab (Untested)

--- core/src/main/java/dev/paintballplus/PaintballPlus.java
+++ core/src/main/java/dev/paintballplus/PaintballPlus.java
@@ -1,16 +1,29 @@
 package dev.paintballplus;
 
 import dev.paintballplus.listener.F5BlockListener;
+import dev.paintballplus.listener.TabIndicatorListener;
+import dev.paintballplus.tab.ModIndicator;
+import dev.paintballplus.tab.PresenceApiClient;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
 
 @AddonMain
 public class PaintballPlus extends LabyAddon<PaintballPlusConfig> {
 
+    private ModIndicator modIndicator;
+
     @Override
     protected void enable() {
         PerspectiveBlocker.initialize(this);
+
+        this.modIndicator = new ModIndicator();
+        PresenceApiClient presenceApiClient = new PresenceApiClient(this.modIndicator);
+        GameDetector gameDetector = new GameDetector();
+
         this.registerListener(new F5BlockListener(this));
+        this.registerListener(gameDetector);
+        this.registerListener(presenceApiClient);
+        this.registerListener(new TabIndicatorListener(this, this.modIndicator));
     }
 
     @Override
